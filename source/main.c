31529d5c3e1713cdbaca4ab5bd0598209c3f6bf3
matth3wmajf <matthewmajf@proton.me>
2025-03-02 22:59:18 -0500

    Implement code for testing IEEE-754 division...

--- source/main.c
+++ source/main.c
@@ -6,15 +6,18 @@
 
 int main()
 {
-	float_t l_a = 1.432551f, l_b = 0.834239f, l_c = 0.0f;
+	float_t l_a = 1.0, l_b = 2.0, l_c = 0.0f;
 
-	l_c = l_a / l_b;
+	printf("Dividing X by 2, 32 times, starting with X being set to 1...\n");
 
-	printf("l_a = %f l_b = %f l_c = %f\n", l_a, l_b, l_c);
+	for(int l_i = 0; l_i < 32; l_i++)
+	{
+		fp32_div((fp32_t*)&l_c, *(fp32_t*)&l_a, *(fp32_t*)&l_b);
 
-	fp32_div((fp32_t*)&l_c, *(fp32_t*)&l_a, *(fp32_t*)&l_b);
+		printf("%.16f / %.16f = %.16f\n", l_a, l_b, l_c);
 
-	printf("l_a = %f l_b = %f l_c = %f\n", l_a, l_b, l_c);
+		l_a = l_c;
+	}
 
 	return 0;
 }
